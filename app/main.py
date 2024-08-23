dbb474a0cdb59745761ee5993892c5bf3bbfb129
suk-6 <me@suk.kr>
2024-08-23 20:19:51 +0900

    feat: Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .dependencies import check_request_server
 from .routers import room
 
 app = FastAPI(dependencies=[Depends(check_request_server)])
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(room)
 
