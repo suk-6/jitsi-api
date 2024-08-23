b9dcbe85f51402077b0d235f52b1f203aab57ba4
suk-6 <me@suk.kr>
2024-08-23 20:19:59 +0900

    feat: Add join, leave, and terminate room endpoints

--- app/routers/room.py
+++ app/routers/room.py
@@ -6,6 +6,26 @@ router = APIRouter(
 )
 
 
+@router.get("/join-check")
+async def join_check():
+    return {"message": "Join Check"}
+
+
 @router.get("/new")
 async def new_room():
     return {"message": "New Room"}
+
+
+@router.get("/join")
+async def join_room():
+    return {"message": "Join Room"}
+
+
+@router.get("/leave")
+async def leave_room():
+    return {"message": "Leave Room"}
+
+
+@router.get("/terminate")
+async def terminate_room():
+    return {"message": "Terminate Room"}
