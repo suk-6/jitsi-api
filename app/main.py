a703483767ede0585f8faee1788633b34dab4ee1
suk-6 <me@suk.kr>
2024-09-01 22:21:02 +0900

    feat: Update CORS origins to use front_url from configuration

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+
 from .dependencies import check_request_server
+from .config import Settings
 from .routers import room
 
 app = FastAPI(dependencies=[Depends(check_request_server)])
+env = Settings()
 
 origins = [
-    "http://localhost:5173",
+    env.front_url,
 ]
 
 app.add_middleware(
