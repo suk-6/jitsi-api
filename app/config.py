36915836b465e7f467b8948610ddb4b6443c0620
suk-6 <me@suk.kr>
2024-09-02 10:28:43 +0900

    refactor: Update app/config.py with environment variables for Postgres and Redis

--- app/config.py
+++ app/config.py
@@ -9,9 +9,27 @@ def get_settings():
 
 
 class Settings(BaseSettings):
+    # HOST
     host: str = "localhost"
     port: int = 8443
+
+    # Frontend URL
     front_url: str = "http://localhost:5173"
+
+    # JWT
     jwt_app_id: str = "jitsi"
     jwt_app_secret: str = "jitsi"
+
+    # Postgres
+    postgres_host: str = "localhost"
+    postgres_port: int = 5432
+    postgres_user: str = "postgres"
+    postgres_password: str = "password"
+    postgres_db: str = "db"
+
+    # Redis
+    redis_host: str = "localhost"
+    redis_port: int = 6379
+
+    # Environment File
     model_config = SettingsConfigDict(env_file=".env")
