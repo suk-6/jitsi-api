91751470c384f8824767aeb2baefe5b7f2ff9592
suk-6 <me@suk.kr>
2024-09-02 10:29:18 +0900

    feat: Initialize SQL database and create session

---
+++ app/databases/sql.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, create_engine, Session
+
+from app.config import Settings
+
+env = Settings()
+
+engine = create_engine(
+    f"postgresql://{env.postgres_user}:{env.postgres_password}@{env.postgres_host}:{env.postgres_port}/{env.postgres_db}"
+)
+
+SQLModel.metadata.create_all(engine)
+
+session = Session(engine)
