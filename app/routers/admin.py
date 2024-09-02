45e918b3617c4ddb08588eb8bf16c3b318edfbcd
suk-6 <me@suk.kr>
2024-09-02 17:13:53 +0900

    feat: Add endpoint to retrieve video file

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 
-from app.services.admin import get_video
+from app.services.admin import get_rooms, get_video
 
 
 router = APIRouter(
     prefix="/admin",
     tags=["admin"],
 )
+
+
+@router.get("/rooms")
+async def get():
+    rooms = await get_rooms()
+    return rooms
 
 
 @router.get("/video", response_class=FileResponse)
