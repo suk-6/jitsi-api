108304a8389989feea98be61f837165241242333
suk-6 <me@suk.kr>
2024-09-02 15:58:35 +0900

    chore: Include admin router in main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.remote.scp import JibriSCP
+
 from .dependencies import check_request_server
 from .config import Settings
-from .routers import room
+from .routers import room, admin
 
 app = FastAPI(dependencies=[Depends(check_request_server)])
 env = Settings()
@@ -21,3 +23,4 @@ app.add_middleware(
 )
 
 app.include_router(room)
+app.include_router(admin)
